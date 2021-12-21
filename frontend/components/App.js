c21c36e75f70b3574d94659628deca26ca76017d
GAMER <cyril.ravot@hotmail.fr>
2021-12-21 16:43:40 +0100

    Mise en place du  début du front react.

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,25 +1,14 @@
-import { useState, useEffect } from 'react'
 import Banner from './Banner'
 import logo from '../assets/Logo-Groupomania'
-import Cart from './Cart'
 import Footer from './Footer'
 import '../styles/Layout.css'
 
 function App() {
-	const savedCart = localStorage.getItem('cart')
-	const [cart, updateCart] = useState(savedCart ? JSON.parse(savedCart) : [])
-	useEffect(() => {
-		localStorage.setItem('cart', JSON.stringify(cart))
-	}, [cart])
-
 	return (
 		<div>
 			<Banner>
 				<img src={logo} alt='logo-groupomania' className='logo' />
 			</Banner>
-			<div className='layout-inner'>
-				<Cart cart={cart} updateCart={updateCart} />
-			</div>
 			<Footer />
 		</div>
 	)
--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,27 +1,14 @@
-import { useState, useEffect } from 'react'
 import Banner from './Banner'
 import logo from '../assets/Logo-Groupomania.png'
-import Cart from './Cart'
-import Footer from './Footer'
 import '../styles/Layout.css'
 
 function App() {
-	const savedCart = localStorage.getItem('cart')
-	const [cart, updateCart] = useState(savedCart ? JSON.parse(savedCart) : [])
-	useEffect(() => {
-		localStorage.setItem('cart', JSON.stringify(cart))
-	}, [cart])
 
 	return (
 		<div>
 			<Banner>
-				<img src={logo} alt='logo-Groupomania' className='logo' />
-				<h1 className='title'>La maison jungle</h1>
+				<img class="navbar-brand" src={logo} alt='logo-Groupomania' className='logo' />
 			</Banner>
-			<div className='layout-inner'>
-				<Cart cart={cart} updateCart={updateCart} />
-			</div>
-			<Footer />
 		</div>
 	)
 }
