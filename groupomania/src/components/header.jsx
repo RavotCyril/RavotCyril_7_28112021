e87bfd0f84bf89c0b2776ba1559652a03bbbc2f0
Cyril Ravot <cyril.ravot@hotmail.fr>
2022-02-04 16:29:23 +0100

    Re initialisation projet react Frontend.

--- groupomania/src/components/header.jsx
+++
@@ -1,6 +0,0 @@
-function Header() {
-	return (<p>"houhou c'est mon header"</p>)
-
-}
-
-export default Header;
--- groupomania/src/index.js
+++ groupomania/src/index.js
@@ -1,30 +1,17 @@
-import logo from './build/Logo-Groupomania.png';
-import './styles/index.css';
-import './styles/Normalize.css';
-import HeaderJB from './components/header'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/index.js</code> and save to reload.
-        </p>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <HeaderJB />
-    </div>
-  );
-}
-
-export default App;
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
